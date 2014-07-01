bfd631050a22dae889b6d4ecf7536c41c0408cc5
Daniel Gorst <dgorst@scottlogic.co.uk>
2014-07-01 10:41:39 +0100

    Adds some interim working for calculating the root of the merkle tree
    This stuff isn't working yet, my guess is some kind of endian issue when
    I'm hashing together nodes in the tree. Committing this to back it up, and
    I'll move onto the next stage of the training for now.

--- block_verifier.js
+++ block_verifier.js
@@ -34,6 +34,61 @@ var padString = function(string)	{
 	return result;
 }
 
+var calculateBottomRowOfMrklTree = function(mrklTree)	{
+	var bottomRow = [];
+	var numberOfRows = 1;
+	var rowSize = 1;
+	var result = 1;
+	while (result < mrklTree.length)	{
+		rowSize *= 2;
+		result += rowSize;
+		++numberOfRows;
+	}
+
+	var bottomRowSize = rowSize;
+	// If our built up tree is larger than the actual tree, we have a node which only has one leaf
+	if (result > mrklTree.length)	{
+		--bottomRowSize;
+	}
+
+	for (var i = 0; i < bottomRowSize; ++i)	{
+		bottomRow[i] = mrklTree[i];
+	}
+	// If we have a node with only one leaf, pad it
+	if (bottomRowSize % 2 !== 0)	{
+		bottomRow[bottomRow.length] = bottomRow[bottomRow.length - 1];
+	}
+	return bottomRow;
+}
+
+var calculateNextRow = function(currentRow)	{
+	var nextRow = [];
+	for (var i = 0; i < currentRow.length; i += 2)	{
+		var node1Le = convertEndian(currentRow[i]);
+		var node2Le = convertEndian(currentRow[i+1]);
+
+
+		var d1Le = doubleHashFunc(node1Le);
+		var d2Le = doubleHashFunc(node2Le);
+		// These values aren't currently right. It looks like some kind of endian issue, but I haven't gotten to the bottom of it yet
+		nextRow[Math.floor(i/2)] = convertEndian(doubleHashFunc(d1Le + d2Le));
+	}
+	return nextRow;
+}
+
+var calculateMrklRoot = function(mrklTree)	{
+	var row = calculateBottomRowOfMrklTree(mrklTree);
+	while (row.length > 1)	{
+		console.log("Row: " + row);
+		row = calculateNextRow(row);
+		console.log("Next Row: " + row);
+	}
+}
+
+// For quick testing, we currently try and calculate the merkle root of our transactions tree if this script is run from Node.
+var block = require("./data/block.json");
+calculateMrklRoot(block.mrkl_tree);
+
 var verifyDataBlock = function(blockFilePath)	{
 	var block = require(blockFilePath);
 
