3626aab381ba16d0ecded72c43f1954e8639c4a1
Daniel Gorst <dgorst@scottlogic.co.uk>
2014-07-07 15:28:28 +0100

    Update our data model so it contains multiple pages of data
    As we go back a page, we load in the previous 10 blocks into an array
    which we prepend to our existing data. Each page of 10 hashes is stored in
    an array, and the arrays are stored within our model. I've now added
    implementations for the next and previous functions within our block list
    controller.

--- block-explorer/app/js/controllers.js
+++ block-explorer/app/js/controllers.js
@@ -7,48 +7,89 @@ var bitCoinUrl = 'http://localhost:8000/bitCoinSite';
 var controllers = angular.module('blockExplorer.controllers', []);
 
 controllers.service('DataModel', ['$rootScope', '$http', function($rootScope, $http) {
+  var currentPageIndex = 0;
+  var currentPageHashes = [];
   var blockHashes = [];
 
   var loadBlockData = function(blockHash)  {
     return $http.get(bitCoinUrl + '/rawblock/' + blockHash);
   };
 
-  var refreshData = function(startingHash)  {
+  var loadPageData = function(startingHash, pageIndex)  {
     var blockDataPromise = loadBlockData(startingHash);
     blockDataPromise.success(function(blockData)  {
-      blockHashes[blockHashes.length] = startingHash;
+      currentPageHashes[currentPageHashes.length] = startingHash;
 
       // We keep loading blocks until we have a chain of 10
-      if (blockHashes.length < 10)  {
+      if (currentPageHashes.length < 10)  {
         console.log('Block loaded. Loading next one...');
-        refreshData(blockData.prev_block);
+        loadPageData(blockData.prev_block, pageIndex);
       }
       // At that point, let our app know that new data is available
       else  {
         console.log('Reached 10 blocks');
         console.log('Broadcasting data update');
-        $rootScope.$broadcast('dataModel::dataUpdated', blockData);
+        blockHashes[pageIndex] = currentPageHashes;
+        $rootScope.$broadcast('dataModel::dataUpdated');
       }
     });
   };
 
   // Kick things off by querying bitcoin explorer for the latest block available
   $http.get(bitCoinUrl + '/q/latesthash').success(function(hash)  {
-        console.log(hash);
-        //refreshData(hash.toLowerCase());
+        loadPageData(hash.toLowerCase());
 
         // For testing, I've got a block hash here which I know contains data
-        refreshData('00000000000000001be85a1d1ef9e276a3638d9d6d9d028c2b9d2b9b77b228ba');
+        //loadPageData('00000000000000001be85a1d1ef9e276a3638d9d6d9d028c2b9d2b9b77b228ba', currentPageIndex);
       }
     );
+
+  this.getPreviousBlocksArray = function()  {
+    if (currentPageIndex === 0) {
+      // Get the hash of the block previous to the last one on this page
+      var lastBlockPromise = loadBlockData(currentPageHashes[currentPageHashes.length - 1]);
+      lastBlockPromise.success(function(blockData)  {
+        var previousBlockHash = blockData.prev_block;
+
+        currentPageHashes = [];
+
+        // Add an empty page to the start of our data
+        blockHashes.unshift([]);
+
+        // Load in the previous page-worth of data
+        loadPageData(previousBlockHash, 0);
+      });
+    }
+    else  {
+      --currentPageIndex;
+      currentPageHashes = blockHashes[currentPageIndex];
+      $rootScope.$broadcast('dataModel::dataUpdated');
+    }
+  };
+
+  this.getNextBlocksArray = function() {
+    if (currentPageIndex < blockHashes.length - 1)  {
+      ++currentPageIndex;
+      currentPageHashes = blockHashes[currentPageIndex];
+      $rootScope.$broadcast('dataModel::dataUpdated');
+    }
+  };
+
+  this.getCurrentPageIndex = function() {
+    return currentPageIndex;
+  };
 
   // We return a promise. The client should handle the success and error cases
   this.getBlockData = function(blockHash) {
     return loadBlockData(blockHash);
   };
 
-  this.getBlocksArray = function()  {
-    return blockHashes;
+  this.getCurrentPageHashes = function()  {
+    return currentPageHashes;
+  };
+
+  this.isLastPage = function()  {
+    return currentPageIndex === blockHashes.length - 1;
   };
 }]);
 
@@ -56,19 +97,21 @@ controllers.controller('BlockListController', ['$scope', '$http', 'DataModel', f
 
     console.log('Loading block list controller');
 
-    $scope.$on('dataModel::dataUpdated', function(event, data) {
+    $scope.$on('dataModel::dataUpdated', function(event) {
         console.log('Data model updated. Updating blocks array');
-        $scope.blocks = DataModel.getBlocksArray();
+        $scope.blocks = DataModel.getCurrentPageHashes();
+        $scope.isLastPage = DataModel.isLastPage();
     });
 
-    $scope.blocks = DataModel.getBlocksArray();
+    $scope.blocks = DataModel.getCurrentPageHashes();
+    $scope.isLastPage = DataModel.isLastPage();
 
     $scope.getPrevious = function() {
-      console.log('Get previous');
+      DataModel.getPreviousBlocksArray();
     };
 
     $scope.getNext = function() {
-      console.log('Get next');
+      DataModel.getNextBlocksArray();
     };
   }]);
 
@@ -84,7 +127,7 @@ controllers.controller('BlockDetailsController', ['$scope', '$routeParams', 'Dat
     };
     updateBlockData(hash);
 
-    $scope.$on('dataModel::dataUpdated', function(event, data) {
+    $scope.$on('dataModel::dataUpdated', function(event) {
         console.log('Data model updated. Updating block data');
         updateBlockData(hash);
     });
