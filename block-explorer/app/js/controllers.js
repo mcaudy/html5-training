ddb581b7085fbd53fc3873cabeda246dcf1da9aa
Daniel Gorst <dgorst@scottlogic.co.uk>
2014-07-07 12:09:12 +0100

    Updates the data stored by our model
    We now just store the hashes we have retrieved rather than storing all
    the block data. This is because the amount of block data stored could get
    very large if we were querying a large number of blocks, for example by
    going back and forward through pages of 10. We currently query the
    bitcoin service each time we choose a block by hash. We could improve
    this by storing block data locally, and then loading it locally as needed.
    
    We no longer use the carousel on the main page, as it makes an assumption
    we know how many pages we have. In reality, we will use next and previous
    buttons to navigate pages, and it is likely that going back will load
    a new page of data. The next and previous buttons are wired up to our
    controller, but the controller doesn't do anything with those commands yet.

--- block-explorer/app/js/controllers.js
+++ block-explorer/app/js/controllers.js
@@ -7,38 +7,49 @@ var bitCoinUrl = 'http://localhost:8000/bitCoinSite';
 var controllers = angular.module('blockExplorer.controllers', []);
 
 controllers.service('DataModel', ['$rootScope', '$http', function($rootScope, $http) {
-  var data = {};
+  var blockHashes = [];
 
-  // Given the hash of a block, get the raw data for it
-    var loadBlockData = function(blockHash)  {
-      $http.get(bitCoinUrl + '/rawblock/' + blockHash).success(function(blockData)  {
-        data[blockHash] = blockData;
+  var loadBlockData = function(blockHash)  {
+    return $http.get(bitCoinUrl + '/rawblock/' + blockHash);
+  };
 
-        // We keep loading blocks until we have a chain of 10
-        if (Object.keys(data).length < 10)  {
-          console.log('Block loaded. Loading next one...')
-          loadBlockData(blockData.prev_block);
-        }
-        // At that point, let our app know that new data is available
-        else  {
-          console.log('Reached 10 blocks');
-          console.log('Broadcasting data update');
-          $rootScope.$broadcast('dataModel::dataUpdated', data);
-        }
-      });
-    };
+  var refreshData = function(startingHash)  {
+    var blockDataPromise = loadBlockData(startingHash);
+    blockDataPromise.success(function(blockData)  {
+      blockHashes[blockHashes.length] = startingHash;
+
+      // We keep loading blocks until we have a chain of 10
+      if (blockHashes.length < 10)  {
+        console.log('Block loaded. Loading next one...');
+        refreshData(blockData.prev_block);
+      }
+      // At that point, let our app know that new data is available
+      else  {
+        console.log('Reached 10 blocks');
+        console.log('Broadcasting data update');
+        $rootScope.$broadcast('dataModel::dataUpdated', blockData);
+      }
+    });
+  };
 
   // Kick things off by querying bitcoin explorer for the latest block available
   $http.get(bitCoinUrl + '/q/latesthash').success(function(hash)  {
         console.log(hash);
-        loadBlockData(hash.toLowerCase());
+        //refreshData(hash.toLowerCase());
 
         // For testing, I've got a block hash here which I know contains data
-        //loadBlockData('00000000000000001be85a1d1ef9e276a3638d9d6d9d028c2b9d2b9b77b228ba');
+        refreshData('00000000000000001be85a1d1ef9e276a3638d9d6d9d028c2b9d2b9b77b228ba');
       }
     );
 
-  return data;
+  // We return a promise. The client should handle the success and error cases
+  this.getBlockData = function(blockHash) {
+    return loadBlockData(blockHash);
+  };
+
+  this.getBlocksArray = function()  {
+    return blockHashes;
+  };
 }]);
 
 controllers.controller('BlockListController', ['$scope', '$http', 'DataModel', function($scope, $http, DataModel) {
@@ -47,36 +58,34 @@ controllers.controller('BlockListController', ['$scope', '$http', 'DataModel', f
 
     $scope.$on('dataModel::dataUpdated', function(event, data) {
         console.log('Data model updated. Updating blocks array');
-        updateBlocksArray();
+        $scope.blocks = DataModel.getBlocksArray();
     });
 
-    var updateBlocksArray = function()  {
-      // Put our blocks into an array to be displayed in our list
-      var blocksArray = [];
-      var index = 0;
-      for (var key in DataModel)  {
-        if (DataModel.hasOwnProperty(key))  {
-          blocksArray[index] = DataModel[key];
-        }
-        ++index;
-      }
-      $scope.blocks = blocksArray;
+    $scope.blocks = DataModel.getBlocksArray();
+
+    $scope.getPrevious = function() {
+      console.log('Get previous');
     };
 
-    updateBlocksArray();
-
-    // TODO: Paging functionality doesn't work yet, but I've added pages to ensure that next and previous controls are displayed. We can work out how to view them later
-    $scope.pages = ['Previous', 'Current', 'Next'];
+    $scope.getNext = function() {
+      console.log('Get next');
+    };
   }]);
 
 controllers.controller('BlockDetailsController', ['$scope', '$routeParams', 'DataModel', function($scope, $routeParams, DataModel) {
   	var hash = $routeParams.hash;
 
-    // Using the name of the block as the key, get the block data from the backing data model
-  	$scope.block = DataModel[hash];
+    var updateBlockData = function(hash)  {
+      // Using the name of the block as the key, get the block data from the backing data model
+      var blockPromise = DataModel.getBlockData(hash);
+      blockPromise.success(function(data) {
+        $scope.block = data;
+      });
+    };
+    updateBlockData(hash);
 
     $scope.$on('dataModel::dataUpdated', function(event, data) {
         console.log('Data model updated. Updating block data');
-        $scope.block = DataModel[hash];
+        updateBlockData(hash);
     });
   }]);
