54cd52a9f4055751aab29724853e595ddea21aa2
Daniel Gorst <dgorst@scottlogic.co.uk>
2014-07-10 14:24:25 +0100

    Moves block visualisation into its own service
    The controllers module was starting to get a little big. We now make the
    block details controller depend on the block visualiser service.

---
+++ block-explorer/app/js/block-visualiser.js
@@ -0,0 +1,132 @@
+'use strict';
+
+var d3module = angular.module('blockExplorer.blockVisualiser', []);
+
+d3module.service('visualiseService', function() {
+	 this.visualiseTransactions = function(transactions)  {
+      // Put our array of transactions into a tree, where the root node has the same 'in' property as its children
+      var treeData = {
+        'in' : transactions,
+        hash: 'This block'
+      };
+
+      // For now, I'm limiting the number of children displayed. Otherwise the tree gets too busy
+      treeData['in'] = treeData['in'].slice(0, 50);
+
+      var svgSize = 960;
+      var treeSize = svgSize * 0.8;
+
+      var tree = d3.layout.tree()
+                  .sort(null)
+                  .size([treeSize, treeSize])
+                  .children(function(d) {
+                    return (!d['in'] || d['in'].length === 0) ? null : d['in'];
+                  });
+
+      var nodes = tree.nodes(treeData);
+      var links = tree.links(nodes);
+
+      // Set zoom behaviour on the svg element. This also seems to enable dragging behaviour. Good times.
+      var zoom = d3.behavior.zoom()
+        .scaleExtent([0.1, 10])
+        .on('zoom', zoomed);
+
+      var svg = d3.select('svg');
+      svg.style('background-color', 'white')
+        .style('width', svgSize)
+        .style('height', svgSize);
+      svg.call(zoom);
+      svg.on('dblclick.zoom', null);
+
+      // Render the tree. We start zoomed out with the tree in the middle of the view. We then zoom in on it
+      var layoutRoot = svg.append('g')
+        .attr('class', 'container')
+        .attr('transform', 'translate(' + treeSize / 3 + ',' + treeSize / 3 + ')scale(0.2,0.2)');
+
+      layoutRoot.transition()
+          .duration(750)
+          .attr('transform', function(d)  { return 'translate(' + svgSize * 0.05 + ',' + svgSize * 0.05 + ')scale(1,1)'; });
+
+      function zoomed() {
+        layoutRoot.attr('transform', 'translate(' + d3.event.translate + ')scale(' + d3.event.scale + ')');
+      }
+
+      // Links between nodes
+      var link = d3.svg.diagonal()
+         .projection(function(d) { return [d.y, d.x]; });
+
+      var linkGroup = layoutRoot.selectAll('path.link')
+         .data(links)
+         .enter()
+         .append('path')
+         .attr('class', 'link')
+         .attr('d', link);
+
+      // Nodes
+      var nodeGroup = layoutRoot.selectAll('g.node')
+        .data(nodes)
+        .enter()
+         .append('g')
+         .attr('class', 'node')
+         .attr('transform', function(d) { return 'translate(' + d.y + ',' + d.x + ')'; })
+         .on('click', click);
+
+      nodeGroup.append('text')
+          .text(function(d) {
+            if (d.hasOwnProperty('hash')) {
+              return d.hash; 
+            }
+            else  {
+              return d.prev_out.hash;
+            }
+          })
+          .attr('dx', 5)
+          .attr('dy', 2)
+          .style('opacity', 0.0);
+
+      nodeGroup.append('circle')
+         .attr('class', 'node-dot')
+         .attr('r', 2);
+
+      // The first node is this block. We label it as such
+      var firstNode = layoutRoot.select('g.node');
+        firstNode.append('text')
+              .text(function(d) {
+                d.showText = true;
+                return d.hash; 
+              } )
+              .attr('dy', -5)
+              .attr('dx', -10);
+
+      function refreshNodes() {
+        // Select all the nodes in the tree, apart from the root node
+        nodeGroup.selectAll('text')
+            .filter(function(d) { return d != treeData; } )
+            // Fade our text in or out
+            .transition()
+              .duration(750)
+              .style('opacity', function(d) {
+                if (d.hasOwnProperty('showText') && d.showText) {
+                  return 1.0;
+                }
+                else  {
+                  return 0.0;
+                }
+            });
+      }
+
+      function click(d) {
+        // If we've clicked on the root node, ignore the click
+        if (d === treeData)  {
+          return;
+        }
+        if (!d.hasOwnProperty('showText') || d.showText === false) {
+          d.showText = true;
+        }
+        else  {
+          d.showText = false;
+        }
+        refreshNodes();
+      }
+    };
+});
--- block-explorer/app/js/controllers.js
+++ block-explorer/app/js/controllers.js
@@ -4,7 +4,7 @@ var bitCoinUrl = 'http://localhost:8000/bitCoinSite';
 
 /* Controllers */
 
-var controllers = angular.module('blockExplorer.controllers', []);
+var controllers = angular.module('blockExplorer.controllers', ['blockExplorer.blockVisualiser']);
 
 controllers.service('DataModel', ['$rootScope', '$http', '$q', function($rootScope, $http, $q) {
   var currentPageIndex = 0;
@@ -130,16 +130,15 @@ controllers.controller('BlockListController', ['$scope', '$http', 'DataModel', f
     };
   }]);
 
-controllers.controller('BlockDetailsController', ['$scope', '$routeParams', 'DataModel', function($scope, $routeParams, DataModel) {
+controllers.controller('BlockDetailsController', ['$scope', '$routeParams', 'DataModel', 'visualiseService', function($scope, $routeParams, DataModel, visualiseService) {
   	var hash = $routeParams.hash;
 
     var updateBlockData = function(hash)  {
       // Using the name of the block as the key, get the block data from the backing data model
       var blockPromise = DataModel.getBlockData(hash);
       blockPromise.then(function(data) {
-        console.log(data);
         $scope.block = data;
-        visualiseTransactions();
+        visualiseService.visualiseTransactions($scope.block.tx);
       });
     };
     updateBlockData(hash);
@@ -148,131 +147,4 @@ controllers.controller('BlockDetailsController', ['$scope', '$routeParams', 'Dat
         console.log('Data model updated. Updating block data');
         updateBlockData(hash);
     });
-
-    var visualiseTransactions = function()  {
-      // Put our array of transactions into a tree, where the root node has the same 'in' property as its children
-      var treeData = {
-        'in' : $scope.block.tx,
-        hash: 'This block'
-      };
-
-      // For now, I'm limiting the number of children displayed. Otherwise the tree gets too busy
-      treeData['in'] = treeData['in'].slice(0, 50);
-
-      var svgSize = 960;
-      var treeSize = svgSize * 0.8;
-
-      var tree = d3.layout.tree()
-                  .sort(null)
-                  .size([treeSize, treeSize])
-                  .children(function(d) {
-                    return (!d['in'] || d['in'].length === 0) ? null : d['in'];
-                  });
-
-      var nodes = tree.nodes(treeData);
-      var links = tree.links(nodes);
-
-      // Set zoom behaviour on the svg element. This also seems to enable dragging behaviour. Good times.
-      var zoom = d3.behavior.zoom()
-        .scaleExtent([0.1, 10])
-        .on('zoom', zoomed);
-
-      var svg = d3.select('svg');
-      svg.style('background-color', 'white')
-        .style('width', svgSize)
-        .style('height', svgSize);
-      svg.call(zoom);
-      svg.on('dblclick.zoom', null);
-
-      // Render the tree. We start zoomed out with the tree in the middle of the view. We then zoom in on it
-      var layoutRoot = svg.append('g')
-        .attr('class', 'container')
-        .attr('transform', 'translate(' + treeSize / 3 + ',' + treeSize / 3 + ')scale(0.2,0.2)');
-
-      layoutRoot.transition()
-          .duration(750)
-          .attr('transform', function(d)  { return 'translate(' + svgSize * 0.05 + ',' + svgSize * 0.05 + ')scale(1,1)'; });
-
-      function zoomed() {
-        layoutRoot.attr('transform', 'translate(' + d3.event.translate + ')scale(' + d3.event.scale + ')');
-      }
-
-      // Links between nodes
-      var link = d3.svg.diagonal()
-         .projection(function(d) { return [d.y, d.x]; });
-
-      var linkGroup = layoutRoot.selectAll('path.link')
-         .data(links)
-         .enter()
-         .append('path')
-         .attr('class', 'link')
-         .attr('d', link);
-
-      // Nodes
-      var nodeGroup = layoutRoot.selectAll('g.node')
-        .data(nodes)
-        .enter()
-         .append('g')
-         .attr('class', 'node')
-         .attr('transform', function(d) { return 'translate(' + d.y + ',' + d.x + ')'; })
-         .on('click', click);
-
-      nodeGroup.append('text')
-          .text(function(d) {
-            if (d.hasOwnProperty('hash')) {
-              return d.hash; 
-            }
-            else  {
-              return d.prev_out.hash;
-            }
-          })
-          .attr('dx', 5)
-          .attr('dy', 2)
-          .style('opacity', 0.0);
-
-      nodeGroup.append('circle')
-         .attr('class', 'node-dot')
-         .attr('r', 2);
-
-      // The first node is this block. We label it as such
-      var firstNode = layoutRoot.select('g.node');
-        firstNode.append('text')
-              .text(function(d) {
-                d.showText = true;
-                return d.hash; 
-              } )
-              .attr('dy', -5)
-              .attr('dx', -10);
-
-      function refreshNodes() {
-        // Select all the nodes in the tree, apart from the root node
-        nodeGroup.selectAll('text')
-            .filter(function(d) { return d != treeData; } )
-            // Fade our text in or out
-            .transition()
-              .duration(750)
-              .style('opacity', function(d) {
-                if (d.hasOwnProperty('showText') && d.showText) {
-                  return 1.0;
-                }
-                else  {
-                  return 0.0;
-                }
-            });
-      }
-
-      function click(d) {
-        // If we've clicked on the root node, ignore the click
-        if (d === treeData)  {
-          return;
-        }
-        if (!d.hasOwnProperty('showText') || d.showText === false) {
-          d.showText = true;
-        }
-        else  {
-          d.showText = false;
-        }
-        refreshNodes();
-      }
-    };
   }]);
